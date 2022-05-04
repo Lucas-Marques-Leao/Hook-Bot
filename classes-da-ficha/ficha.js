c0ed911025f3e39a74cd5c39357ac6b3b2893cc0
LucasMarquesLeao <lucasmarquesleao@outlook.com>
2022-05-04 16:54:02 -0300

    Comandos na pasta commands: não funfa

---
+++ classes-da-ficha/ficha.js
@@ -0,0 +1,326 @@
+
+const charSheet  =  class Ficha {
+    constructor(nom, raç, cla){
+        this.nome = nom
+        this.raça = raç
+        this.classePri = cla
+        this.nivelPri = 1
+        this.nivel = 1
+        this.classeSec = "Nenhuma"
+        this.nivelSec = 0
+        this.idade = 12
+        this.setAtributos(10, 10, 10, 10, 10, 10)
+        this.inventario = []
+        this.saude = 10
+        this.saudeTemp = 0
+        this.nivelConj = 0
+
+        this.proficiencia()
+    }
+
+    getNome(){
+        return this.nome
+    }
+
+    setNome(nom){
+        this.nome = nom
+    }
+
+    getRaça(){
+        return this.raça
+    }
+
+    setRaça(raç){
+        this.raça = raç
+    }   
+
+    getClassePri(){
+        return this.classePri
+    }
+
+    setClassePri(class1){
+        this.classePri = class1
+    }
+
+    getClasseSec(){
+        return this.classeSec
+    }
+
+    setClasseSec(class2){
+        this.classeSec = class2
+    }
+
+    getNivelPri(){
+        return this.nivelPri
+    }
+
+    setNivelPri(nPri){
+        this.nivelPri = nPri
+        this.nivel = this.nivelPri + this.nivelSec
+        this.proficiencia()
+    }
+
+    getNivelSec(){
+        return this.nivelSec
+    }
+
+    setNivelSec(nSec){
+        this.nivelSec = nSec
+        this.nivel = this.nivelPri + this.nivelSec
+        this.proficiencia()
+    }
+
+    getNivel(){
+        return this.nivel
+    }
+
+    getNivelConj(){
+        return this.nivelConj
+    }
+
+    setNivelConj(magic){
+        this.nivelConj = magic
+    }
+
+    getInventario(){
+        return this.inventario
+    }
+
+    setInventario(item){
+        this.inventario.push(item)
+    }
+
+    getIdade(){
+        return this.idade
+    }
+
+    setIdade(age){
+        this.idade = age
+    }
+
+    setAtributos(strength, dexterity, constitution, intelligence, wisdom, charisma){
+        this.strAt = strength
+        this.dexAt = dexterity
+        this.conAt = constitution
+        this.intAt = intelligence
+        this.wisAt = wisdom
+        this.chaAt = charisma
+        this.atribuirmodificador()
+    }
+
+    getStrAt(){
+        return this.strAt
+    }
+
+    setStrAt(strength){
+        this.strAt = strength
+        this.atribuirmodificador()
+    }
+
+    getDexAt(){
+        return this.dexAt
+    }
+
+    setDexAt(dexterity){
+        this.dexAt = dexterity
+        this.atribuirmodificador()
+    }
+
+    getConAt(){
+        return this.conAt
+    }
+
+    setConAt(constitution){
+        this.conAt = constitution
+        this.atribuirmodificador()
+    }
+
+    getIntAt(){
+        return this.intAt
+    }
+
+    setIntAt(intelligence){
+        this.intAt = intelligence
+        this.atribuirmodificador()
+    }
+
+    getWisAt(){
+        return this.wisAt
+    }
+
+    setWisAt(wisdom){
+        this.wisAt = wisdom
+        this.atribuirmodificador()
+    }
+
+    getChaAt(){
+        return this.chaAt
+    }
+
+    setChaAt(charisma){
+        this.chaAt = charisma
+        this.atribuirmodificador()
+    }
+
+    getSaude(){
+        return this.saude
+    }
+
+    setSaude(hp){
+        this.saude = hp
+    }
+
+    getSaudeTemp(){
+        return this.saudeTemp
+    }
+
+    setSaudeTemp(hpt){
+        this.saudeTemp = hpt
+    }
+
+    getProf(){
+        return this.prof
+    }
+
+    getStrMod(){
+        return this.strMod
+    }
+
+    getDexMod(){
+        return this.dexMod
+    }
+
+    getConMod(){
+        return this.conMod
+    }
+
+    getIntMod(){
+        return this.intMod
+    }
+
+    getWisMod(){
+        return this.wisMod
+    }
+
+    getChaMod(){
+        return this.chaMod
+    }
+
+
+    atribuirmodificador(){
+        let modifier = ['-5', '-4', '-4', '-3' , '-3', '-2' , '-2', '-1','-1', '0' , '0', '+1', '+1', '+2', '+2', '+3', '+3', '+4', '+4','+5', '+5', '+6', '+6', '+7', '+7', '+8', '+8', '+9', '+9', '+10']
+        
+         this.strMod = modifier[(this.strAt - 1)]
+         this.dexMod = modifier[(this.dexAt - 1)]
+         this.conMod = modifier[(this.conAt - 1)]
+         this.intMod = modifier[(this.intAt - 1)]
+         this.wisMod = modifier[(this.wisAt - 1)]
+         this.chaMod = modifier[(this.chaAt - 1)]
+    }   
+
+    proficiencia(){
+        let profic = ['+2', '+2', '+2', '+2', '+3', '+3', '+3', '+3', '+4', '+4', '+4', '+4', '+5', '+5', '+5', '+5', '+6', '+6', '+6', '+6', '+7', '+7', '+7', '+7', '+8', '+8', '+8', '+8', '+9', '+9']
+
+        this.prof = profic[(this.nivel - 1)]
+    }
+    //Loucuras de Conjuração para Multiclasses com Magia(apenas até 2 classes diferentes)
+    setConjClass1PactMagic(){
+        this.nivelConj = "Magia de Pacto"
+    }
+    
+    setConjClass1Full(){
+        this.nivelConj = this.nivelPri
+
+    }
+
+    setConjClass1Half(){
+        this.nivelConj = parseInt(this.nivelPri * 1 / 2)
+
+    }
+    
+    setConjClass1Third(){
+        this.nivelConj = parseInt(this.nivelPri * 1 / 3)
+
+    }
+
+    setConjClass2Full(){
+        this.nivelConj = this.nivelSec
+    }
+
+    setConjClass2Half(){
+        this.nivelConj = parseInt(this.nivelSec * 1 / 2)
+    }
+
+    setConjClass2Third(){
+        this.nivelConj = parseInt(this.nivelSec * 1 / 3)
+    }
+
+    setConjClass1FullClass2Full(){
+        this.nivelConj = (this.nivelPri  + this.nivelSec)
+    }
+
+    setConjClass1FullClass2Half(){
+        this.nivelConj = parseInt(this.nivelPri + (this.nivelSec * 1 / 2))
+    }
+
+    setConjClass1FullClass2Third(){
+        this.nivelConj = parseInt(this.nivelPri + (this.nivelSec * 1 / 3))
+    }
+
+    setConjClass1HalfClass2Full(){
+        this.nivelConj = parseInt((this.nivelPri * 1 / 2) + this.nivelSec)
+    }
+
+    setConjClass1HalfClass2Half(){
+        this.nivelConj = parseInt((this.nivelPri * 1 / 2) + (this.nivelSec * 1 / 2))
+    }
+
+    setConjClass1HalfClass2Third(){
+        this.nivelConj = parseInt((this.nivelPri * 1 / 2) + (this.nivelSec *1 / 3))
+    }
+
+    setConjClass1ThirdClass2Full(){
+        this.nivelConj = parseInt((this.nivelPri * 1 / 3) + this.nivelSec)
+    }
+
+    setConjClass1ThirdClass2Half(){
+        this.nivelConj = parseInt((this.nivelPri * 1 / 3) + (this.nivelSec * 1 / 2))
+    }
+
+    setConjClass1ThirdClass2Third(){
+        this.nivelConj = parseInt((this.nivelPri * 1 / 3) + (this.nivelSec * 1 / 3))
+    }
+
+    pornaTelaATabela(){
+        this.informacoes = 
+        `
+         Nome: ${this.getNome()} 
+         Nível: ${this.getNivel()}
+         Bônus de Proficiência: ${this.getProf()}
+         Raça: ${this.getRaça()} | Idade: ${this.getIdade()} 
+         Classe Primária: ${this.getClassePri()} | Nível Primário: ${this.getNivelPri()} 
+         Classe Secundária: ${this.getClasseSec()} | Nível Secundário: ${this.getNivelSec()} 
+         
+         Nível de Conjurador: ${this.getNivelConj()}
+         Pontos de Vida: ${this.getSaude()} | Pontos de Vida Temporários: ${this.getSaudeTemp()}
+         
+         Inventário: ${this.getInventario()}
+          
+         Atributos: Força: ${this.getStrAt()}
+                     Destreza: ${this.getDexAt()}
+                     Constituição: ${this.getConAt()}
+                     Inteligência: ${this.getIntAt()}
+                     Sabedoria: ${this.getWisAt()}
+                     Carisma: ${this.getChaAt()} 
+        
+         Modificadores: Modificador de Força: ${this.getStrMod()}
+                    Modificador de Destreza: ${this.getDexMod()}
+                    Modificador de Constituição: ${this.getConMod()}
+                    Modificador de Inteligência: ${this.getIntMod()}
+                    Modiicador de Sabedoria: ${this.getWisMod()}
+                    Modificador de Carisma: ${this.getChaMod()}`
+    }
+
+}
+
+module.exports = {charSheet}
--- index.js
+++ index.js
@@ -1,27 +1,143 @@
-const Discord = require('discord.js')
+const { Client, Collection, Intents } = require('discord.js')
 const { token } = require('./config.json')
+/*const { charSheet } = require('./classes-da-ficha/ficha.js')*/
+const fs = require('node:fs')
 
+/*
+//definições
+let c1 = new charSheet('Rayer', 'Humano', 'Sacerdote')
+const Rayer = c1.getNome()
+let c2 = new charSheet('Klaus', 'Tielfing', 'Bruxo')
+const Klaus = c2.getNome()
+let c3 = new charSheet('Eris', 'Humana', 'Maga')
+const Eris = c3.getNome()
+let c4 = new charSheet('Fulano', 'Tiefling', 'Patrulheiro')
+const Fulano = c4.getNome()
 
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]})
+//settings
+c1.setNivelPri(7)
+c1.setIdade("24 anos")
+c1.setAtributos(9, 12, 10, 16, 22, 14)
+c1.setInventario("Símbolo Sagrado")
+c1.setConjClass1Full()
+
+c2.setNivelPri(18)
+c2.setIdade("17 anos")
+c2.setAtributos(10, 10, 16, 14, 16, 20)
+c2.setInventario("Lâmina Espectral")
+c2.setInventario("Grimório Demoníaco")
+c2.setConjClass1PactMagic()
+
+c3.setNivelPri(3)
+c3.setIdade("15 anos")
+c3.setAtributos(7, 12, 9, 20, 18, 15)
+c3.setClasseSec("Feiticeira")
+c3.setNivelSec(7)
+c3.setInventario("Cajado")
+c3.setInventario("Pergaminho de Passo do Trovão(5º círculo)")
+c3.setConjClass1FullClass2Full()
+
+//infopass
+c1.pornaTelaATabela()
+c2.pornaTelaATabela()
+c3.pornaTelaATabela()
+c4.pornaTelaATabela()
+*/
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
+
+client.commands = new Collection()
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 
 client.once('ready', () =>{
 	console.log(`${client.user.username} tá ON!`)
 })
 
-client.on('messageCreate',  interaction =>{
+/*client.on('messageCreate',  interaction =>{
 	
-	if (interaction.content === '/pica'){
-		interaction.reply(`suas bolas foram lustradas, sinta-se à vontade ${interaction.author}`)
-	}else if (interaction.content === '/rola'){
+	 if (interaction.content === '/rola'){
 		interaction.reply(`que falta de vergonha! UMA ROULA`)
 	}else if (interaction.content === '/deus'){
 		interaction.reply(`${interaction.author}, nem tente, você jamais vai chegar aos pés do Deus EDNALDO PEREIRA`)
 		interaction.channel.send({files: ["./fotos/pisque.jpg"]})
-	}else if (interaction.content ==='/ping'){
-		interaction.reply(`O ping do Bot é ${client.ws.ping}ms`)
+	}else if (interaction.content ==='/shura'){
+		interaction.reply("O Gladiador Mascarado, o Primeiro Deus de Athas")
+		interaction.channel.send({files: ["./fotos/shura.jpg"]})
+		
+	}else if (interaction.content.startsWith(`/${Rayer.toLowerCase()}`)){
+		if (interaction.content === `/${Rayer.toLowerCase()} status`){
+			interaction.reply(`Ficha do ${Rayer}:
+			${c1.informacoes}`)
+		}else if (interaction.content === `/${Rayer.toLowerCase()} longsword`){
+			let d20 = Math.floor(Math.random() * 20) + 1
+			let res = d20 + Number(c1.getProf().replace('+', '')) + Number(c1.getChaMod().replace('+', '').replace('-', ''))
+			interaction.reply(`atacou com ${d20 + c1.getProf() + c1.getChaMod()}(${res}) para acertar a Classe de Armadura!`)
+		}else{
+			interaction.reply("Escreve certinho, fazendo favor...")
+		}
+		
+	}else if (interaction.content.startsWith(`/${Klaus.toLowerCase()}`)){
+		if (interaction.content === `/${Klaus.toLowerCase()} status`){
+			interaction.reply(`Ficha do ${Klaus}:
+			${c2.informacoes}`)
+		}else if (interaction.content === `/${Klaus.toLowerCase()} eldblast`){
+			let d20 = Math.floor(Math.random() * 20) + 1
+			let res = d20 + Number(c2.getProf().replace('+', '')) + Number(c2.getChaMod().replace('+', ''))
+			interaction.reply(`atacou com ${d20 + c2.getProf() + c2.getChaMod()}(${res}) para acertar a Classe de Armadura!`)
+		}else if (interaction.content === `/${Klaus.toLowerCase()} lamspec`){
+			let d20 = Math.floor(Math.random() * 20) + 1
+			let res = d20 + Number(c2.getProf().replace('+', '')) + Number(c2.getChaMod().replace('+', '')) + 3
+			interaction.reply(`atacou com ${d20  + c2.getProf() + c2.getChaMod()}+3(${res}) para acertar a Classe de Armadura!`)
+		}else{
+			interaction.reply("Pare de parasitar o chat, Analfabeto!")
+		}
+		
+	}else if (interaction.content.startsWith(`/${Eris.toLowerCase()}`)){
+		if (interaction.content === `/${Eris.toLowerCase()} status`){
+			interaction.channel.send({files: ["./fotos/eris.jpg"]})
+			interaction.reply(`Ficha da ${Eris}:
+			${c3.informacoes}`)
+			
+		}else if (interaction.content === `/${Eris.toLowerCase()} firebolt`){
+			let d20 = Math.floor(Math.random() * 20) + 1
+			let res = d20 + Number(c3.getProf().replace('+', '')) + Number(c3.getIntMod().replace('+', '').replace('-', ''))
+			interaction.reply(`atacou com ${d20 + c3.getProf() + c3.getIntMod()}(${res}) para acertar a Classe de Armadura!`)
+		}else{
+			interaction.reply("Que pena, mas não se preocupa! Eu vou te ensinar a escrever algum dia!")
+		}
+	}else if (interaction.content.startsWith(`/${Fulano.toLowerCase()}`)){
+		if (interaction.content === `/${Fulano.toLowerCase()} status`){
+			interaction.reply(`Ficha do ${Fulano}:
+			${c4.informacoes}`)
+		}else{
+			interaction.reply("Esperava mais de você...")
+		}
 	}
-})
+
+})*/
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 
 client.login(token)
